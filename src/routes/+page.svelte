78d7e6986c3184aebacd63d943ebbf2857cf30f4
Dullinger-g <dullinger.gabriel@johak.at>
2024-02-27 14:13:59 +0100

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import {supabase} from '$lib/supabase';
+
+    let promise = supabase.from("countries").select();
+</script>
+
+{#await promise}
+<div>...loading</div>
+{:then result}
+<div>{JSON.stringify(result)}</div>
+{/await}
