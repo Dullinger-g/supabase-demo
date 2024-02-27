391250913e43c866516e538d083e22116f897c29
Dullinger-g <dullinger.gabriel@johak.at>
2024-02-27 14:41:17 +0100

    Länder auswahl

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,18 @@
     import {supabase} from '$lib/supabase';
 
     let promise = supabase.from("countries").select();
+    let countries = [];
+    promise.then(result => {
+        countries = result.data;
+    });
 </script>
 
 {#await promise}
 <div><span class="loading loading-dots loading-sm"></span></div>
-{:then result}
-<div>{JSON.stringify(result)}</div>
+{:then}
+<ul>
+    {#each countries as country}
+        <li>{country.name}</li>
+    {/each}
+</ul>
 {/await}
